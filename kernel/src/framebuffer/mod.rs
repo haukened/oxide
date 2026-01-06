59ecb3e38d19b468593aff4cff32675e4827cf9e
David Haukeness <david@hauken.us>
2026-01-06 16:23:03 +0000

    feat: add draw_rect and panic_screen functions to framebuffer module

--- kernel/src/framebuffer/mod.rs
+++ kernel/src/framebuffer/mod.rs
@@ -1,11 +1,30 @@
 use oxide_abi::Framebuffer;
 
+mod boot;
 mod draw;
+
+pub use boot::BootStage;
+pub use draw::FramebufferColor;
 
 pub fn clear_framebuffer(fb: &Framebuffer) -> Result<(), ()> {
     draw::clear_black(fb)
 }
 
-pub fn draw_boot_marker(fb: &Framebuffer) -> Result<(), ()> {
-    draw::draw_boot_marker(fb)
+pub fn draw_rect(
+    fb: &Framebuffer,
+    start_x: usize,
+    start_y: usize,
+    size_x: usize,
+    size_y: usize,
+    color: FramebufferColor,
+) -> Result<(), ()> {
+    draw::draw_rect(fb, start_x, start_y, size_x, size_y, color)
+}
+
+pub fn panic_screen(fb: &Framebuffer) -> ! {
+    draw::panic_screen(fb);
+}
+
+pub fn draw_boot_stage(fb: &Framebuffer, stage: boot::BootStage) {
+    boot::draw_boot_stage(fb, stage);
 }
